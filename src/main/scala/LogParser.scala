bd1190df8b0d7d4c71d5e8013d8932a908b1c607
Alex Iliev <alex.iliev@gmail.com>
2011-11-15 22:47:05 +0000

    Add test file.

--- src/main/scala/LogParser.scala
+++ src/main/scala/LogParser.scala
@@ -5,9 +5,9 @@ import java.{io=>jio}
 import org.joda.time.format.DateTimeFormat
 import org.joda.time.DateTime
 
-case class LogNode(time:DateTime)
-case class Target(t2:DateTime, name:String) extends LogNode(t2)
-case class Module(t2:DateTime, name:String, targets:List[Target]) extends LogNode(t2)
+sealed trait LogNode
+case class Target(name:String, start:DateTime, end:DateTime) extends LogNode
+case class Module(name:String, targets:List[Target]) extends LogNode
 
 object LogParser extends RegexParsers
 {
@@ -15,7 +15,7 @@ object LogParser extends RegexParsers
     DateTimeFormat.forPattern("HH:mm")
 
   def main(args:Array[String]) = {
-    System.out.println(parse(all, new jio.InputStreamReader(System.in)).get.mkString("\n"))
+    System.out.println(parse(all, new jio.FileReader("src/test/resources/test.txt")).get.mkString("\n"))
   }
 
 //  override val whiteSpace = "\n"r
@@ -24,20 +24,20 @@ object LogParser extends RegexParsers
   val linechars = ("[^\n]*"r)
   val eol = "\n"
   val timestamp =  ("""\d\d:\d\d"""r) ~ " " ^^
-                      { case t ~ _ => TIME_PARSER.parseDateTime(t) }
+            { case t ~ _ => TIME_PARSER.parseDateTime(t) }
   val h1_text = "end h1:"
   val h2_text = "h2:"
   val h1 = timestamp ~ h1_text ~ linechars ~ eol ^^
             { case t ~ _ ~ title ~ _ => (t,title) }
   val h2 = timestamp ~ h2_text ~ linechars ~ eol ^^
-            { case t ~ _ ~ title ~ _ => Target(t,title) }
-  val genline = timestamp ~ (not (h1_text | h2_text)) ~ linechars ~ eol
-//  val line = h1 | h2 | genline
+            { case t ~ _ ~ title ~ _ => (t,title) }
+  val genline = timestamp ~ (not (h1_text | h2_text)) ~ linechars ~ eol ^^
+            { case t ~ _ ~ _ ~ _ => t }
   val target = h2 ~ (genline*) ^^
-            { case h2 ~ _ => h2 }
+            { case ((start,title)) ~ times =>
+              Target(title, start, times.lastOption.getOrElse(start)) }
   val module = (target*) ~ h1 ^^
-            { case ts ~ ((t,title)) => Module(t, title, ts) }
-//  val all = repsep(line, "\n")
+            { case ts ~ ((t,title)) => Module(title, ts) }
   val all = module+
   
 }
